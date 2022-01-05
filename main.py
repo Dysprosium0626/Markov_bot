679cc35e0b277bd5415f9ee5ef8c50af5ef13f91
DysprosiumDy <59086724+Dysprosium0626@users.noreply.github.com>
2022-01-05 08:43:24 +0800

    frontend

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'index'
+    return 'Please go to \'/login\' for more information'
 
 
 @app.route('/login', methods=['POST', 'GET'])
