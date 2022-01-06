14b663a0d71a064bd21a52669aac9e2984284bb4
DysprosiumDy <59086724+Dysprosium0626@users.noreply.github.com>
2022-01-07 00:25:15 +0800

    update logo, slogan, github hyperlink and re-design frontend

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, url_for, request, render_template, redirect
 from service.article_service import ArticleService
 from service.text_serivce import TextService
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 
 @app.route('/')
@@ -13,8 +13,31 @@ def index():
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'GET':
-        return render_template('login.html')
+        return render_template('login.html', image="../logo/markov_grey.png")
+    else:
+        theme = request.form.get('theme')
+        number = int(request.form.get('number'))
+        if request.files["myfile"].filename == '':
+            article_service = ArticleService(theme, number)
+            result = article_service.generate_text()
+        else:
+            try:
+                file = request.files["myfile"]
+                str = bytes.decode(file.read(), encoding="utf-8")
+                text_service = TextService(number)
+                result = text_service.generate_text(str)
+            except Exception:
+                return render_template('login.html', str="请上传.txt文件")
+
+        return render_template('login.html', str=result)
+
+
+@app.route('/loginen', methods=['POST', 'GET'])
+def loginen():
+    if request.method == 'GET':
+        return render_template('loginen.html')
     elif request.method == 'POST':
+        print(request.form.get('theme'))
         theme = request.form.get('theme')
         number = int(request.form.get('number'))
         if request.files["myfile"].filename == '':
@@ -25,10 +48,17 @@ def login():
                 file = request.files["myfile"]
                 str = bytes.decode(file.read(), encoding="utf-8")
             except UnicodeDecodeError:
-                return render_template('login.html', str="请上传.txt文件")
+                return render_template('loginen.html', str="请上传.txt文件")
             text_service = TextService(number)
             result = text_service.generate_text(str)
-        return render_template('login.html', str=result)
+        return render_template('loginen.html', str=result)
+
+
+@app.route('/github', methods=['POST', 'GET'])
+def github():
+    if request.method == 'GET':
+        return redirect("https://github.com/Dysprosium0626/Markov_bot")
+
 
 
 if __name__ == "__main__":
